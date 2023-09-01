08fc5908b08b3a05478769dbd1ad1d0542cb6b09
maciekQQ <mika.maciej1101@gmail.com>
2023-09-01 22:23:31 +0200

    Sharing state using React Context

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import "./App.css";
+import { Dispatch } from "react";
 
 import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
 import Counter from "./state-management/Counter";
 import TaskList from "./state-management/TaskList";
 import LoginStatus from "./state-management/LoginStatus";
+import { useReducer } from "react";
+import tasksReducer from "./state-management/reducers/tasksReducer";
+import NavBar from "./routing/NavBar";
+import HomePage from "./routing/HomePage";
+import TasksContext from "./state-management/contexts/tasksContext";
 
 function App() {
-  return <LoginStatus />;
+  const [tasks, dispatch] = useReducer(tasksReducer, []);
+  return (
+    <>
+      <TasksContext.Provider value={{ tasks, dispatch }}>
+        <NavBar />
+        <HomePage />
+      </TasksContext.Provider>
+    </>
+  );
 }
 
 export default App;
