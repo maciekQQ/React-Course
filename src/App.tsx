05f0ca54f3ae0788ca29f399626db5ca4f70b52a
maciekQQ <mika.maciej1101@gmail.com>
2023-09-01 18:27:52 +0200

    Creating Complex Actions

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,10 @@ import "./App.css";
 import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
 import Counter from "./state-management/Counter";
+import TaskList from "./state-management/TaskList";
 
 function App() {
-  return <Counter />;
+  return <TaskList />;
 }
 
 export default App;
