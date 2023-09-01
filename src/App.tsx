075465b1b836bb4da61d8038cde8b54d4d957149
maciekQQ <mika.maciej1101@gmail.com>
2023-09-01 18:50:06 +0200

    LoginStatus Reducer

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,10 @@ import TodoForm from "./react-query/TodoForm";
 import TodoList from "./react-query/TodoList";
 import Counter from "./state-management/Counter";
 import TaskList from "./state-management/TaskList";
+import LoginStatus from "./state-management/LoginStatus";
 
 function App() {
-  return <TaskList />;
+  return <LoginStatus />;
 }
 
 export default App;
